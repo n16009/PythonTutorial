897e04d4d8b496230022ee500f9c02e8c79083bc
yuka yoshida <n16009@std.it-college.ac.jp>
2016-07-22 09:46:48 +0900

    課題

---
+++ 22.py
@@ -0,0 +1,59 @@
+import random
+
+
+def you_hand():
+    while True:
+        print("あなたの手 : "),
+        hand = int(input())
+        if hand == 1 or hand == 2 or hand == 3: break
+        print("1~3の数字を入力してください")
+    return hand
+
+
+def check_hand(y, c):
+    if y == c:
+        if y == 1: print("共にグーで",)
+        if y == 2: print("共にチョキで",)
+        if y == 3: print("共にパーで",)
+        print("引き分けです")
+        return 0
+    else:
+        if y == 1:
+            if c == 2:
+                print("あなた:グー, COM:チョキ...YOU WIN!!")
+                return 1
+            if c == 3:
+                print("あなた:グー, COM:パー...YOU LOSE!!")
+                return -1
+        if y == 2:
+            if c == 1:
+                print("あなた:チョキ, COM:グー...YOU LOSE!!")
+                return -1
+            if c == 3:
+                print("あなた:チョキ, COM:パー...YOU WIN!!")
+                return 1
+        if y == 3:
+            if c == 1:
+                print("あなた:パー, COM:グー...YOU WIN!!")
+                return 1
+            if c == 2:
+                print("あなた:パー, COM:チョキ...YOU LOSE!!")
+                return -1
+def calc_rate(d, w, a):
+    if a == d: p = 0
+    else: p = w / (a - d)
+    return p
+
+
+if __name__ == "__main__":
+    draw, win, all = 0.0, 0.0, 0.0
+    print("じゃんけんゲームスタート")
+    print("グー:1 ,チョキ:2 ,パー:3を入力してください")
+    while True:
+        p = calc_rate(draw, win, all)
+        you = you_hand()
+        com = random.randint(1, 3)
+        tmp = check_hand(you, com)
+        if tmp == 0: draw += 1
+        if tmp == 1: win += 1
+        again = input()
--- janken/play.py
+++ janken/play.py
@@ -4,22 +4,51 @@ HANDS = ['グー', 'チョキ', 'パー']
 
 
 def select_hand():
-    """
-    コンピュータの手をランダムに決める
-    :return: HANDSの中のいずれか。
-    """
-    import random
-    pass
+    while True:
+        print("あなたの手"),
+        HANDS = input()
+        if HANDS == 1 or HANDS == 2 or HANDS == 3: break
+    return 0
 
 
-def judgement(player, computer):
-    """
-    じゃんけんの勝敗を判定する。
-    :param player: HANDSの中のどれか。
-    :param computer: HANDSの中のどれか。
-    :return: プレイヤーが勝ちの場合は1,あいこは0,負けは-1を返す。
-    """
-    pass
+def judgement():
+    if player == computer:
+        if player == 1: print("共にグーで"),
+        if player == 2: print("共にチョキで"),
+        if player == 3: print("共にパーで"),
+        print("引き分け")
+        return HANDS
+    else:
+        if player == '1':
+            if computer == '2':
+                print("あなた:グー, COM:チョキ...あなたの勝ち!!")
+                return +1
+            if computer == '3':
+                print("あなた:グー, COM:パー...あなたの負け!!")
+                return -1
+        if player == '2':
+            if computer == '1':
+                print("あなた:チョキ, COM:グー...あなたの負け!!")
+                return -1
+            if computer =='3':
+                print("あなた:チョキ, COM:パー...あなたの勝ち!!")
+                return +1
+        if player == '3':
+            if computer == '1':
+                print("あなた:パー, COM:グー...あなたの勝ち!!")
+                return +1
+            if computer == '2':
+                print("あなた:パー, COM:チョキ...あなたの負け!!")
+                return -1
+
+
+"""
+じゃんけんの勝敗を判定する。
+:param player: HANDSの中のどれか。
+:param computer: HANDSの中のどれか。
+:return: プレイヤーが勝ちの場合は1,あいこは0,負けは-1を返す。
+"""
+pass
 
 
 def save_score(result):
@@ -32,9 +61,25 @@ def save_score(result):
     pass
 
 
+def calc_rate(d, w, a):
+    if a == d: p = 0
+    else: p = w / (a - d)
+    return p
+
+
 if __name__ == '__main__':
+    draw, win, all = 0.0, 0.0, 0.0
     player = int(input('グー(1)/チョキ(2)/パー(3)を選んでください(数字): '))
-    computer = select_hand()
-    result = judgement(player, computer)
-    # コンピュータの手と勝敗の結果を表示
+    computer = random.randint(1, 3)
+    result = judgement()
+    while True:
+        p = calc_rate(draw, win, all)
+        com = random.randint(1, 3)
+        tmp = judgement()
+        if tmp == 0: draw += 1
+        if tmp == 1: win += 1
+        again = input()
+
+
+     # コンピュータの手と勝敗の結果を表示
     save_score(result)
