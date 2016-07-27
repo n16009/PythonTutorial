8a1130bf4e54014721d69e26ab3a7ca33af78aea
yuka yoshida <n16009@std.it-college.ac.jp>
2016-07-27 10:10:31 +0900

    課題

---
+++ aoj.py
@@ -0,0 +1,7 @@
+# a, b = [int(i) for i in input().split()]
+W, H, x, y, r = [int(i) for i in input().split()]
+
+data = [int(i) for i in input().split()]
+
+data,.ort()
+print("{0}{1}{2}".format(data[0],data[1],data[2]))
